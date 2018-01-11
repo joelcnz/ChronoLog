715b841e1b8e4cd6bfb580c917e4249b47b5e00f
Joel Christensen <joelcnz@gmail.com>
2018-01-11 20:11:30 +1300

    Woops

---
+++ source/gui.d
@@ -0,0 +1,328 @@
+import std.conv: text, to;
+
+import dlangui;
+
+import jmisc;
+
+import control, taskman;
+
+struct Gui {
+private:
+	Window _window;
+	EditBox _editBoxMain;
+	TextWidget _textWidgetCategory;
+	EditLine _editLineId,
+		_editLineAddCat,
+		_editLineComment,
+		_editLineDate,
+		_editLineTime,
+		_editLineEndTime,
+		_editLineDuration;
+	CheckBox _checkBoxTime,
+			_checkBoxEndTime,
+			_checkBoxAddCat;
+
+    TaskMan _taskMan;
+    Control _control;
+
+    string _input;
+public:
+    auto getInput() {
+        return _input;
+    }
+
+	int setup(ref TaskMan taskMan) {
+        _taskMan = taskMan;
+        _control.setup(_taskMan);
+
+		_window = Platform.instance.createWindow(
+			"Time Log", null, WindowFlag.Resizable, 1280, 800);
+
+		// Crease widget to show in window
+		_window.mainWidget = parseML(q{
+			HorizontalLayout {
+				//#0000FF #C0E0E070
+				backgroundColor: "#7070FF0C" // Lime green
+				HorizontalLayout {
+					VerticalLayout {
+						margins: 3
+						padding: 3
+
+						EditBox {
+							id: editBoxMain
+							minWidth: 1100; minHeight: 700; maxHeight: 640;
+						}
+
+						Button { id: buttonActivate; maxWidth: 100; text: "- Activate Last Line -" }
+
+						HorizontalLayout {
+							TextWidget { text: "Category:" }
+							TextWidget {
+								id: textWidgetCategory
+								text: "(none set)"
+							}
+						}
+
+						HorizontalLayout {
+							TextWidget { text: "Comment:" }
+							EditLine {
+								id: editLineComment
+								minWidth: 900; maxWidth: 900
+								text: ""
+							}
+
+							Button { id: buttonTest; text: "Command"; }
+						}
+					}
+					VerticalLayout {
+						HorizontalLayout {
+							TextWidget { text: "Ref:" }
+							EditLine { id: editLineId; text: "0"; minWidth: 100; maxWidth: 100 }
+						}
+
+						Button { id: buttonGet; maxWidth: 100; text: "Get" }
+
+						TextWidget { text: "Add categories:" }
+						HorizontalLayout { EditLine { id: editLineAddCat; text: ""; minWidth: 100; maxWidth: 100 } CheckBox { id: checkBoxAddCat; } }
+
+						TextWidget { text: "Date:" }
+						EditLine { id: editLineDate; text: "1 1 2018"; minWidth: 100; maxWidth: 100 }
+						TextWidget { text: "Time:" }
+						HorizontalLayout { EditLine { id: editLineTime; text: "0 0 0"; minWidth: 100; maxWidth: 100 } CheckBox { id: checkBoxTime; } }
+						TextWidget { text: "End Time:" }
+						HorizontalLayout { EditLine { id: editLineEndTime; text: "0 0 0"; minWidth: 100; maxWidth: 100 } CheckBox { id: checkBoxEndTime; } }
+						TextWidget { text: "Duration:" }
+						EditLine { id: editLineDuration; text: "0 0 0"; minWidth: 100; maxWidth: 100 }
+						
+						Button { id: buttonSet; maxWidth: 100; text: "Set" }
+
+					}
+				}
+			}
+		});
+
+		_editBoxMain = _window.mainWidget.childById!EditBox("editBoxMain");
+		_editLineId = _window.mainWidget.childById!EditLine("editLineId");
+		_editLineAddCat = _window.mainWidget.childById!EditLine("editLineAddCat");
+		_textWidgetCategory = _window.mainWidget.childById!TextWidget("textWidgetCategory");
+		_editLineDate = _window.mainWidget.childById!EditLine("editLineDate");
+		_editLineTime = _window.mainWidget.childById!EditLine("editLineTime");
+		_editLineEndTime = _window.mainWidget.childById!EditLine("editLineEndTime");
+		_editLineDuration = _window.mainWidget.childById!EditLine("editLineDuration");
+		_editLineComment = _window.mainWidget.childById!EditLine("editLineComment");
+		_checkBoxTime = _window.mainWidget.childById!CheckBox("checkBoxTime");
+		_checkBoxEndTime = _window.mainWidget.childById!CheckBox("checkBoxEndTime");
+		_checkBoxAddCat = _window.mainWidget.childById!CheckBox("checkBoxAddCat");
+		
+//<<<<<<< HEAD
+//<<<<<<< HEAD
+		_window.mainWidget.childById!Button("buttonTest").click = delegate(Widget w) {
+			EditLine _editLineSpot;
+			
+			Window _spotWin;
+			
+			_spotWin = Platform.instance.createWindow(
+				"Command", null, WindowFlag.Resizable, 800, 50);
+
+			_spotWin.mainWidget = parseML(q{
+				HorizontalLayout {
+					backgroundColor: "#C0E0E070" // semitransparent yellow background
+					HorizontalLayout {
+						TextWidget {
+							text: "Enter command:"
+						}
+						EditLine {
+							id: editLineSpot
+							minWidth: 500
+						}
+						Button {
+							id: buttonAction
+							text: "Action"
+						}
+					}
+				}
+			});
+
+			_editLineSpot = _spotWin.mainWidget.childById!EditLine("editLineSpot");
+
+			_spotWin.mainWidget.childById!Button("buttonAction").click = delegate(Widget w) {
+				auto output = _control.processInput(_editLineSpot.text.to!string);
+
+				if (output.length > 0) {
+					_editBoxMain.text = _editBoxMain.text ~ (output ~ "\n").to!dstring;
+//=======
+//=======
+//>>>>>>> parent of 489dc14... Failed proper dialog box
+		import std.datetime: DateTime, Clock;
+//>>>>>>> parent of 489dc14... Failed proper dialog box
+
+					return true;
+				}
+
+				return false;
+			};
+
+			return true;
+		};
+
+//<<<<<<< HEAD
+//=======
+		_window.mainWidget.childById!Button("buttonTest").click = delegate(Widget w) {
+			EditLine _editLineSpot;
+			
+			Window _spotWin;
+			
+			_spotWin = Platform.instance.createWindow(
+				"Command", null, WindowFlag.Resizable, 800, 50);
+
+			_spotWin.mainWidget = parseML(q{
+				HorizontalLayout {
+					backgroundColor: "#C0E0E070" // semitransparent yellow background
+					HorizontalLayout {
+						TextWidget {
+							text: "Enter command:"
+						}
+						EditLine {
+							id: editLineSpot
+							minWidth: 500
+						}
+						Button {
+							id: buttonAction
+							text: "Action"
+						}
+					}
+				}
+			});
+
+			_editLineSpot = _spotWin.mainWidget.childById!EditLine("editLineSpot");
+
+			_spotWin.mainWidget.childById!Button("buttonAction").click = delegate(Widget w) {
+				auto output = _control.processInput(_editLineSpot.text.to!string);
+
+				if (output.length > 0) {
+					_editBoxMain.text = _editBoxMain.text ~ (output ~ "\n").to!dstring;
+
+					return true;
+				}
+
+				return false;
+			};
+
+/+
+<<<<<<< HEAD
+>>>>>>> parent of 489dc14... Failed proper dialog box
+=======
+>>>>>>> parent of 489dc14... Failed proper dialog box
++/
+			_spotWin.show();
+
+			return true;
+		};
+
+		_window.mainWidget.childById!Button("buttonActivate").click = delegate(Widget w) {
+			import std.string: lastIndexOf;
+			import std.conv: to;
+
+            auto i = _editBoxMain.text.to!string.lastIndexOf("\n") + 1;
+            _input = _editBoxMain.text[i .. $].to!string;
+            (`"` ~ _input ~ `"`).gh;
+            auto output = _control.processInput(_input);
+            if (output.length > 0)
+                _editBoxMain.text = _editBoxMain.text ~ (output ~ "\n").to!dstring;
+
+            return true;
+		};
+		_window.mainWidget.childById!Button("buttonGet").click = delegate(Widget w) {
+
+            int id;
+			try {
+				id = _editLineId.text.to!int;
+			} catch(Exception e) {
+				return false;
+			}
+
+			auto task = _taskMan.getTask(id); 
+			if (task is null) {
+				_editBoxMain.text = _editBoxMain.text ~ "\nTask out of range!";
+
+				return false;
+			} else {
+				_textWidgetCategory.text = task.id().to!dstring ~ " " ~ task.taskString.to!dstring;
+
+				_editLineDate.text = text(task.dateTime.day, " ", cast(int)task.dateTime.month, " ", task.dateTime.year).to!dstring;
+
+				_checkBoxTime.checked = task.displayTimeFlag;
+				if (_checkBoxTime.checked)
+					_editLineTime.text = text(task.dateTime.hour, " ", task.dateTime.minute, " ", task.dateTime.second).to!dstring;
+				else
+					_editLineTime.text = ""d;
+
+				_checkBoxEndTime.checked = task.displayEndTimeFlag;
+				if (_checkBoxEndTime.checked)
+					_editLineEndTime.text = text(task.endTime.hour, " ", task.endTime.minute, " ", task.endTime.second).to!dstring;
+				else
+					_editLineEndTime.text = ""d;
+				
+				if (task.timeLength.hours ==0 && task.timeLength.minutes == 0 && task.timeLength.seconds == 0)
+					_editLineDuration.text = ""d;
+				else
+					_editLineDuration.text = text(task.timeLength.hours, " ", task.timeLength.minutes, " ", task.timeLength.seconds).to!dstring;
+
+				_editLineComment.text = task.comment().to!dstring;
+			}
+
+            return true;
+		};
+
+		_window.mainWidget.childById!Button("buttonSet").click = delegate(Widget w) {
+			import std.string: split, join, format;
+			auto loadValues() {
+				//             Set Date
+				//             | Start time
+				//             | | End time
+				//             | | | length of time
+				//             | | | | comment
+				//             | | | | |  
+				return format(`%s%s%s%sc"%s"`,
+						text(`sd"`, _editLineDate.text, `" `),
+						_checkBoxTime.checked ? text(`st"`, _editLineTime.text, `" `) : "",
+						_checkBoxEndTime.checked ? text(`et"`, _editLineEndTime.text, `" `) : "",
+						_editLineDuration.text.split.join(" ") != "0 0 0" ? text(`l"`, _editLineDuration.text, `" `) : "",
+						_editLineComment.text).to!string;
+			}
+
+			if (_checkBoxAddCat.checked) {
+				_editBoxMain.text = _editBoxMain.text ~ text("\n",
+					_editLineAddCat.text.split.join(" "), " ",
+					loadValues()).to!dstring;
+			} else {
+				int[] selection;
+				try {
+					selection = _editLineId.text.split.to!(int[]);
+				} catch(Exception e) {
+					_editBoxMain.text = _editBoxMain.text ~ "\nFailed!"d;
+
+					return false;
+				}
+
+				_control.processInput(loadValues, selection);
+				/+
+				foreach(s; selection) {
+					_control.processInput("s" ~ s.to!string);
+					_control.processInput(loadValues, s);
+					_editBoxMain.text = _editBoxMain.text ~ text("s", s, "\n", loadValues, "\n").to!dstring;
+				}
+				+/
+			}
+
+            return true;
+		};
+
+		_editBoxMain.text = "Insert 'h' at the bottom line,\n" ~
+			"then press the Activate button for help\n".to!dstring;
+
+		_window.show();
+
+		return 0;
+    }
+}
---
+++ source/taskman.d
@@ -0,0 +1,473 @@
+//#how did the goto get here?
+//#new
+//#Possible tasks
+//#cannot understand the next two resualts
+//#don't know why it works
+//#not try catch
+//# set new done tasks
+module taskman;
+
+private {
+	import std.stdio;
+	import core.stdc.stdio;
+	import std.string;
+//	import std.date;
+	import std.datetime;
+	import std.file: FileException;
+	import std.algorithm;
+	import std.conv: text;
+
+//	import terminal;
+	import jtask.taskmanbb, jtask.basebb;
+	import base, task;
+}
+
+class TaskMan {
+private:
+	Task[] _possibleTasks, // possible tasks are the tasks you choose from
+		_doneTasks; // done tasks are the tasks that you have done
+	int _selectedTaskIndex; // or gotten task to go with 'g' at command module
+	//#Possible tasks
+	struct TaskHidden {
+		int tagNumber;
+		string tagName;
+		
+		this( int tagNumber, string tagName ) {
+			this.tagNumber = tagNumber;
+			this.tagName = tagName;
+		}
+	}
+	TaskHidden[] tasksHidden;
+	string _cformat; // custom format
+	string _textTank;
+public:
+	@property {
+		string textTank() { return _textTank; }
+		void textTank(string textTank0) { _textTank = textTank0; }
+		string cformat() { return _cformat; }
+	}
+
+	auto listFoundText(in string phrase) {
+		string result;
+
+		int numberOfItem = 1; //#redundant, why not use cast(int)(i + 1)
+		
+		foreach ( i, task; _doneTasks ) {
+			if ( std.string.indexOf(task.comment(), phrase) != -1 ) {
+				immutable tex = task.viewInfo( numberOfItem, cast(int)i, Collum.straitDown, TaskType.done, _cformat );
+				_textTank ~= tex;
+				result ~= tex;
+				numberOfItem++;
+			}
+		}
+
+		return result;
+	}
+
+	string printDay(int fd, int fm, int fy, int tod = 0, int tom = 0, int toy = 0) {
+		import std.range;
+
+		string result;
+		
+		int numberOfItem = 1;
+		bool found = false;
+		bool last = false;
+		int id, im, iy;
+
+		id=fd;
+		im=fm;
+		iy=fy;
+		if (tod != 0)
+			while(true) { // while
+				foreach(i, task; _doneTasks)
+						with(task.dateTime)
+							if (day == id && month == im && year == iy) {
+								immutable info = task.viewInfo(
+									numberOfItem, cast(int)i, Collum.straitDown, TaskType.done, _cformat );
+								_textTank ~= info;
+								result ~= info;
+								numberOfItem++;
+								found = true;
+							}
+
+				id++;
+				if (id > 31) {
+					id = 0;
+					im++;
+					if (im>12) {
+						im = 0;
+						iy++;
+					}
+				}
+				if (last)
+					break;
+				if (id == tod && im == tom && iy == toy)
+					last = true; //#how did the goto get here?
+			} // while
+
+		if (tod == 0)
+			foreach(i, task; _doneTasks) {
+				with(task.dateTime)
+					if (day == fd && month == fm && year == fy) {
+						immutable info = task.viewInfo(
+							numberOfItem, cast(int)i, Collum.straitDown, TaskType.done, _cformat );
+						_textTank ~= info;
+						result ~= info;
+						numberOfItem++;
+						found = true;
+					}
+			}
+
+		if (! found)
+			result = "No results!";
+		
+		return result;
+	} // printDay
+
+	void saveTextTank(string fileName) {
+		immutable fileWrite = "w";
+
+		File(fileName, fileWrite).write(_textTank);
+	}
+
+	void resetCategorys() {
+		_possibleTasks.length = 0;
+	}
+
+	void clearHidden() {
+		tasksHidden.length = 0;
+	}
+	
+	string showHiddenCategorys() {
+		string result;
+
+		foreach(taskh; tasksHidden)
+			with(taskh) {
+				writefln("%03s %s", tagNumber, tagName);
+				result ~= format("%03s %s", tagNumber, tagName);
+			}
+		
+		return result;
+	}
+
+	/// return: the number of possible tasks
+	size_t getNumberOfPossibleTasks()
+	{
+		return _possibleTasks.length;
+	}
+	
+	/// set the time of day
+	auto setTime(int hour, int minute,int second) {
+		_doneTasks[ _selectedTaskIndex ].setTime(hour, minute, second);
+
+		return format("Set time/start time H:M:S %s:%s:%s", hour, minute, second);
+	}
+
+	/// set the end time of day
+	auto setEndTime(int hour, int minute, int second) {
+		_doneTasks[ _selectedTaskIndex ].setEndTime(hour, minute, second);
+
+		return format("End time H:M:S %s:%s:%s", hour, minute, second);
+	}
+
+	/// set time length for task
+	auto setTimeLength( TimeLength tl )
+	{
+		_doneTasks[ _selectedTaskIndex ].setTimeLength( tl );
+
+		return text(tl);
+	}
+
+	bool noPossibleTasks()
+	{
+		return _possibleTasks.length == 0;
+	}
+
+	size_t numberOfTasks()
+	{
+		return _doneTasks.length;
+	}
+
+	void addPossible(Task possibleTask)
+	{
+		_possibleTasks ~= possibleTask;
+	}
+
+	void opOpAssign(string op)(Task doneTask)
+		if (op == "~")
+	{
+		_doneTasks ~= doneTask;
+	}
+
+	Task getPossibleTask(int index) {
+		if ( index >= 0 && index < _possibleTasks.length )
+			return _possibleTasks[index];
+		else {
+			//writecln( Color.red, "You are in error!" );
+			writeln("taskman.d - Task getPossibleTask(int index) - getPossibleTask out of bounds!");
+			return null;
+		}
+	}
+	
+	void saveHidden(in string fileName) {
+		import std.file;
+
+		immutable fileWriteOnly = "w";
+		auto f = File(fileName, fileWriteOnly);
+
+		foreach(taskh; tasksHidden)
+			f.writefln("%03s %s", taskh.tagNumber, taskh.tagName);
+
+		f.close();
+	}
+	
+
+	Task getTask(in int index)
+	{
+		if ( index < 0 || index >= _doneTasks.length)
+		{
+			writeln("Task getTask" ~ "(immutable int index)" ~ " - out of bounds (", index, ")");
+			return null;
+		}
+			
+		return _doneTasks[index];
+	}
+	
+	void setTaskIndex(in int index)
+	{
+		if ( index < 0 || index > _doneTasks.length)
+		{
+			writeln("value for index is out of bounds in _doneTasks. (", index, ")");
+			return;
+		}
+		_selectedTaskIndex = index;
+	}
+		
+	/// set date
+	auto setDate(int date, int month, int year) {
+		_doneTasks[_selectedTaskIndex].setDate(date, month, year);
+
+		return format("\nid: %s, d:m:y %s:%s:%s", _selectedTaskIndex, date, month, year);
+	}
+	
+	/// Add some thing said about the log entry (eg. comment)
+	void setComment(string comment)
+	{
+		_doneTasks[_selectedTaskIndex].setComment(comment);
+	}
+	
+	void viewLast() {
+		if ( _doneTasks.length > 0 )
+			writefln("%2s - %-40s",
+					_doneTasks[$-1].id, _doneTasks[$-1].taskString);
+	}
+	
+	/// Sort list by date and time
+	void doSort() {
+		sort!("a.getDateTime() < b.getDateTime()")(_doneTasks);
+	}
+	
+	//#new
+	void customFormatList(string cformat) {
+		_cformat = cformat;
+	}
+	
+	void convertToCommands(string fileName) {
+		string content;
+		foreach ( i, task; _doneTasks ) {
+			with(task) {
+				content ~= format("%s\n%s\n",
+								  catagoryToCommand(),
+								  dateToCommand());
+				if (comment != "")
+					content ~= commentToCommand() ~ "\n";
+
+				if (timeLength != TimeLength(0,0,0))
+					content ~= lengthToCommand() ~ "\n";
+
+				if (displayTimeFlag)
+					content ~= timeToCommand() ~ "\n";
+
+				if (displayEndTimeFlag)
+					content ~= endTimeToCommand() ~ "\n";
+			}
+		}
+		File(fileName, "w").write(content);
+		//auto f = File(fileName, "w"); // open for writing
+		//f.write(content);
+		//f.close;
+	}
+	
+	/// View all possible tasks
+	auto view( TaskType taskType, int format = 0 ) {
+		string result;
+
+		auto tasks = [_possibleTasks, _doneTasks, _doneTasks][taskType];
+		final switch ( taskType )
+		{
+			case TaskType.possibles:
+				bool isHidden( string testTask ) {
+					foreach( taskString; tasksHidden )
+						if ( testTask == taskString.tagName )
+							return true;
+					return false;
+				}
+				//#this does not help for not displaying the possible tasks
+				Task[] displayTasks;
+				int[] index;
+				foreach( i, t; tasks )
+					if ( t.taskString != "<skip>" && !isHidden( t.taskString ) ) {
+						displayTasks ~= t;
+						index ~= cast(int)i;
+					}
+
+				//#cannot understand the next two resualts
+				//mixin( trace( "displayTasks.length" ) );
+				//mixin( trace( "tasks.length" ) );
+				switch(format) {
+					default: break;
+					case 0:
+						bool isDevOfTwo() { return displayTasks.length % 2; }
+						foreach ( i; 0 .. displayTasks.length / 2 + ( isDevOfTwo ? 1 : 0 ) ) {
+							result ~= displayTasks[ i ].viewInfo(
+								0,
+								index[i],
+								Collum.left,
+								TaskType.possibles
+							); // left
+							if ( displayTasks.length / 2 + i < tasks.length ) {
+								result ~= displayTasks[ cast(uint)displayTasks.length / 2 + i ].viewInfo(
+									0, cast(int)displayTasks.length / 2 + cast(int)i, Collum.right, TaskType.possibles ); // right
+							} else
+								writeln();
+						}
+					break;
+					case 1:
+						foreach(i; 0 .. displayTasks.length) {
+							result ~= displayTasks[i].viewInfo(
+								0,
+								index[i],
+								Collum.left, // ?
+								TaskType.possibles
+							) ~ "\n";
+						}
+					break;
+				}
+			break;
+			case TaskType.done:
+				int offSet = tasks.length > 30 ? cast(int)tasks.length - 30 : 0;
+				foreach ( i, task; tasks[ offSet .. $ ] )
+				{
+					immutable tex = task.viewInfo( cast(int)i, offSet + cast(int)i, Collum.straitDown, TaskType.done, _cformat );
+					_textTank ~= tex;
+					result ~= tex;			
+				}
+			break;
+			case TaskType.allDone:
+				foreach(i, task; tasks) {
+					immutable tex = task.viewInfo( cast(int)i, cast(int)i, Collum.straitDown, TaskType.done, _cformat );
+					_textTank ~= tex;
+					result ~= tex;
+				}
+			break;
+		}
+
+		return result;
+	}
+	
+	/// I/O output
+	void saveToTextFile( in string fileName )
+	{
+		string content;
+		foreach ( i, task; _doneTasks )
+		{
+			content ~= task.doneString( 0, cast(int)i ) ~ '\n';
+		}
+		auto f = File( fileName, "w"); // open for writing
+		f.write( content );
+		f.close;
+	}
+	
+	/// remove from the done tasks list
+	auto removeAt(int index)
+	{
+		string result = "\n";
+
+		if ( index < _doneTasks.length && index >= 0 )
+		{
+			result ~= "Deleting (enter 'ld' to revert to last save) - " ~ _doneTasks[index].viewInfo(0, index, TaskType.done);
+			_doneTasks = _doneTasks[0 .. index] ~ _doneTasks[index + 1 .. $];
+		}
+		else
+		{
+			result ~= "That, my friend, is an invalid number.";
+		}
+
+		return result;
+	}
+	
+//		void viewInfo( int indexNumber, int collum, TaskType taskType = TaskType.possibles )
+
+	/// List just by the selected type (eg. 'got up', but its number)
+	auto listByType( int typeId, string cformat )
+	{
+		string result = "\n";
+
+		int numberOfItem = 1;
+
+		foreach ( i, task; _doneTasks )
+		{
+			if ( task.id == typeId )
+			{
+				immutable tex = task.viewInfo( numberOfItem, cast(int)i, Collum.straitDown, TaskType.done, _cformat );
+				_textTank ~= tex;
+				result ~= tex;
+				numberOfItem++;
+			}
+		}
+
+		return result;
+	}
+
+	void clearDoneTasks()
+	{
+		_doneTasks.length = 0;
+	}
+	
+	void saveDoneTasks(string filename)
+	{
+		TaskManbb tmbb;
+		//	this( int  id, string taskString, TimeLength length0, string comment, DateTime dateTime,
+//		  bool displayTimeFlag ) {
+
+		// loop through all the tasks and add them to tmbb.tasksbb
+		foreach( task; _doneTasks ) {
+			scope( failure ) {
+				writeln( "id: ", task.id );
+			}
+			with( task ) {
+				tmbb.tasksbb ~= new Taskbb( id, _possibleTasks[ id ].taskString, timeLength, comment,
+					                        dateTime, displayTimeFlag, endTime, displayEndTimeFlag );
+			}
+		}
+		tmbb.saveDoneTasksbb( filename );
+	}
+	
+	/// Load tasks that are done
+	void loadDoneTasks(string filename) {
+		_doneTasks.length = 0;
+		TaskManbb tmbb;
+		tmbb.loadDoneTasksbb( filename );
+		foreach( taskbb; tmbb.tasksbb ) {
+			with( taskbb )
+				_doneTasks ~= new Task( id, taskString, timeLength, comment
+				, dateTime, displayStartTimeFlag
+				, endTime, displayEndTimeFlag );
+		}
+	}
+	
+	//#Possible tasks
+	void addHidden( int tagNumber, string tagName ) {
+		tasksHidden ~= TaskHidden( tagNumber, tagName );
+	}
+}
